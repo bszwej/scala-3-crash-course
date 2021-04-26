03e4afc7e86f1247552ca9bb349ab61fb8b479d0
Bartłomiej Szwej <bartek@moia.io>
2021-04-26 12:18:41 +0200

    renaming

---
+++ src/main/scala/c00-about-me.scala
@@ -0,0 +1,13 @@
+/**
+ * About me
+ * 
+ * - Software engineer & a big fan of Scala and FP
+ * - I've been using Scala for over 5 years
+ * - I blog about Scala (bszwej.medium.com)
+ * - Currently working at MOIA
+ * 
+ * You can find me on:
+ * - Twitter (@bszwej)
+ * - Github (@bszwej)
+ *
+ */
